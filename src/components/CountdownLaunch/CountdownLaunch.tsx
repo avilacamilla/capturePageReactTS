8732b8bb6eac5b1f063a65b0499bb8d30382fb63
Camilla Avila <avilacamilla@outlook.com>
2023-11-06 12:18:17 -0300

    style: estiliza texto do container esquerdo, add gradiente e opacidade no texto

--- src/components/CountdownLaunch/CountdownLaunch.tsx
+++ src/components/CountdownLaunch/CountdownLaunch.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './countdownLaunch.css';
 
 const CountdownLaunch = () => {
     return(
-        <div className="countdownLaunch container">
-            <div className="titleCountdown container">
+        <div className="countdownLaunch-container">
+            <div className="titleCountdown-container">
                 <p className="titleDaysLeft">FALTAM</p>
-                <h2 className="titleNumberOfDaysLeft">03 DIAS</h2>
+                <h2 className="titleNumberOfDaysLeft">3 DIAS</h2>
                 <p className="titleDaysLeft">PARA O LANÇAMENTO</p>
             </div>
         </div>
